bc201cde5797702b56df8d70296ff4c81a9ac4fc
cyberdeeb <83480723+cyberdeeb@users.noreply.github.com>
2022-05-07 18:19:45 -0700

    PSET 1

---
+++ deep/deep.py
@@ -0,0 +1,12 @@
+def deep():
+    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
+    if answer == "42":
+        print("Yes")
+    elif answer == "forty-two":
+        print("Yes")
+    elif answer == "forty two":
+        print("Yes")
+    else:
+        print("No")
+
+deep()
